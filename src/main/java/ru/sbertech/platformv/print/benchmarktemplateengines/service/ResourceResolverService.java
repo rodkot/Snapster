5176ce5493e7f68d0b0d9dda69f131c63b8207ac
Родион Котов <kotov.r.v@sbertech.ru>
2025-01-14 17:42:20 +0700

    DCGN-3957 Добавил сервис для работы с ресурсами

---
+++ src/main/java/ru/sbertech/platformv/print/benchmarktemplateengines/service/ResourceResolverService.java
@@ -0,0 +1,48 @@
+package ru.sbertech.platformv.print.benchmarktemplateengines.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ResourceResolverService {
+
+    public String pathDirResource(String path) {
+        Path originalPath = Paths.get(path);
+        Path directoryPath = originalPath.getParent();
+
+        return directoryPath.toString();
+    }
+
+    public String nameResource(String path) {
+        Path originalPath = Paths.get(path);
+        Path filename = originalPath.getFileName();
+
+        return filename.toString();
+    }
+
+    public String readResourceFile(String path) throws IOException {
+        ClassLoader classLoader = ResourceResolverService.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(path)) {
+            assert inputStream != null;
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                return content.toString();
+            }
+        }
+    }
+}
