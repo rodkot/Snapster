e8521fe8522dc6fcf263912e42f28d738b476eb3
Родион Котов <kotov.r.v@sbertech.ru>
2025-01-24 10:28:59 +0700

    DCGN-4159 Рефакторинг шаблона company.ftl

--- src/main/java/ru/sbertech/platformv/print/benchmarktemplateengines/model/dto/ProjectDto.java
+++ src/main/java/ru/sbertech/platformv/print/benchmarktemplateengines/model/dto/ProjectDto.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import liqp.parser.Inspectable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDto implements Inspectable, Serializable {
     private Long id;
     private String name;
