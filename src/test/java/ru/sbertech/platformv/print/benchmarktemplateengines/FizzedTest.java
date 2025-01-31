fa6ef88465aaf76b0d3dce962bf565250377128f
Родион Котов <kotov.r.v@sbertech.ru>
2025-01-31 16:48:00 +0700

    DCGN-4159 Добавил benchmark тестирование для шаблона FizzedEngine

--- src/test/java/ru/sbertech/platformv/print/benchmarktemplateengines/FizzedTest.java
+++ src/test/java/ru/sbertech/platformv/print/benchmarktemplateengines/FizzedTest.java
@@ -2,14 +2,18 @@ package ru.sbertech.platformv.print.benchmarktemplateengines;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.google.common.base.Stopwatch;
+
 import freemarker.template.TemplateException;
 import ru.sbertech.platformv.print.benchmarktemplateengines.model.dto.CompanyDto;
 import ru.sbertech.platformv.print.benchmarktemplateengines.templateengines.impl.FizzedEngine;
+import ru.sbertech.platformv.print.benchmarktemplateengines.templateengines.impl.FreemarkerEngine;
 
 public class FizzedTest extends ExpectedOutputTest {
 
@@ -24,5 +28,25 @@ public class FizzedTest extends ExpectedOutputTest {
     public void testOutput() throws IOException, TemplateException {
         FizzedEngine engine =  FizzedEngine.of(companies);
         assertOutput(output,engine.process());
+    }
+
+    @Test
+    public void benchmarkWithOutOptimizations() throws IOException, TemplateException {
+        Stopwatch sw = Stopwatch.createStarted();
+        for (int i =0; i< 1000; i++){
+            var engine =  FizzedEngine.of(companies);
+            engine.process();
+        }
+        System.out.println(sw.elapsed(TimeUnit.MILLISECONDS)+ " ms.");
+    }
+
+    @Test
+    public void benchmarkWithOptimizations() throws IOException, TemplateException {
+        Stopwatch sw = Stopwatch.createStarted();
+        var engine =  FizzedEngine.of(companies);
+        for (int i =0; i< 1000; i++){
+            engine.process();
+        }
+        System.out.println(sw.elapsed(TimeUnit.MILLISECONDS)+" ms.");
     }
 }
