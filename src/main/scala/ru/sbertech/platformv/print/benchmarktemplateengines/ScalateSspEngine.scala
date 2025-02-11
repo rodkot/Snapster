041df010c88be6fd1342bdc64f0fd80a8815b579
Родион Котов <kotov.r.v@sbertech.ru>
2025-02-11 21:25:21 +0700

    DCGN-3957 Исправил ошибку ClassCastException для шаблона ScalateSspEngine

--- src/main/scala/ru/sbertech/platformv/print/benchmarktemplateengines/ScalateSspEngine.scala
+++ src/main/scala/ru/sbertech/platformv/print/benchmarktemplateengines/ScalateSspEngine.scala
@@ -6,7 +6,7 @@ import org.fusesource.scalate.{TemplateEngine, TemplateSource}
 import ru.sbertech.platformv.print.benchmarktemplateengines.model.dto._
 import ru.sbertech.platformv.print.benchmarktemplateengines.templateengines.StringReportEngine
 
-class ScalateSspEngine(val report: String, val companies: java.util.List[CompanyDto]) extends StringReportEngine{
+class ScalateSspEngine(val report: String, val companies: java.util.List[CompanyDto]) extends StringReportEngine {
 
   override def process(): String = {
     val templateEngine = new TemplateEngine()
@@ -16,8 +16,8 @@ class ScalateSspEngine(val report: String, val companies: java.util.List[Company
     val templateSource = TemplateSource.fromText("test.ssp", report)
     val template = templateEngine.load(templateSource)
 
-    val env = scala.Predef.Map("companies" -> companies.asScala)
+    val env = scala.Predef.Map("companies" -> companies.asScala.toList)
 
-      templateEngine.layout("", template, env)
+    templateEngine.layout("", template, env)
   }
 }
