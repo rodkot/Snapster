4d640d5ac7bd1f000c60c7dbdc0927b65dc26df6
Родион Котов <kotov.r.v@sbertech.ru>
2025-02-17 13:35:04 +0700

    DCGN-3957 Вернул обратно BarberEngine

---
+++ benchmark/src/test/java/ru/sbertech/platformv/print/benchmark/templateengine/BarberTest.java
@@ -0,0 +1,29 @@
+package ru.sbertech.platformv.print.benchmark.templateengine;
+
+import freemarker.template.TemplateException;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import ru.sbertech.platformv.print.benchmark.domain.model.dto.CompanyDto;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BarberTest extends ExpectedOutputTest{
+    @Autowired
+    @Qualifier("reportBarber")
+    private String report;
+
+    @Autowired
+    @Qualifier("outputBarber")
+    private String output;
+
+    @Autowired
+    private List<CompanyDto> companies;
+
+    @Test
+    public void testOutput() throws IOException, TemplateException {
+        var engine = BarberEngine.Companion.of(report, companies);
+        assertOutput(output,engine.process());
+    }
+}
