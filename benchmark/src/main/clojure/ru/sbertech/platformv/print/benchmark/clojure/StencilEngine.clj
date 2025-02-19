1255d6d17d9b911ad727008556c27a01778dce48
Родион Котов <kotov.r.v@sbertech.ru>
2025-02-19 11:26:15 +0700

    DCGN-3957 Добавил удаление выходных файлов для benchmark тестирования StencilEngine

--- benchmark/src/main/clojure/ru/sbertech/platformv/print/benchmark/clojure/StencilEngine.clj
+++ benchmark/src/main/clojure/ru/sbertech/platformv/print/benchmark/clojure/StencilEngine.clj
@@ -1,5 +1,8 @@
 (ns ru.sbertech.platformv.print.benchmark.clojure.StencilEngine
-  (:require [stencil.api :as api]))
+  (:require [stencil.api :as api]
+            [clojure.java.io :as io]))
 
 (defn process [template-path output-path data]
-    (api/render! (api/prepare template-path) (into {} data) :output output-path))
+  (when (.exists (io/file output-path))
+    (io/delete-file output-path))
+  (api/render! (api/prepare template-path) (into {} data) :output output-path))
