098860da380b1b3de585b819a9cdbc53097702bb
Родион Котов <kotov.r.v@sbertech.ru>
2025-02-25 10:37:56 +0700

    DCGN-3957 Рефакторинг структуры проекта для добавления конвертаторов

--- benchmark/src/test/java/ru/sbertech/platformv/print/benchmark/common/SetupTest.java
+++ benchmark/src/test/java/ru/sbertech/platformv/print/benchmark/common/SetupTest.java
@@ -1,8 +1,4 @@
-package ru.sbertech.platformv.print.benchmark.templateengine;
-
-import static org.junit.Assert.*;
-
-import java.util.Locale;
+package ru.sbertech.platformv.print.benchmark.common;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -10,17 +6,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import ru.sbertech.platformv.print.benchmark.common.SetupTest;
 import ru.sbertech.platformv.print.benchmark.domain.configuration.BenchmarkTemplateEnginesAutoConfiguration;
 import ru.sbertech.platformv.print.benchmark.domain.mapper.CompanyMapperImpl;
 import ru.sbertech.platformv.print.benchmark.domain.mapper.EmployeeMapperImpl;
@@ -32,11 +24,43 @@ import ru.sbertech.platformv.print.benchmark.domain.repository.ProjectRepository
 import ru.sbertech.platformv.print.benchmark.domain.service.CompanyService;
 import ru.sbertech.platformv.print.benchmark.domain.service.OfficeService;
 import ru.sbertech.platformv.print.benchmark.domain.service.ResourceResolverService;
+import ru.sbertech.platformv.print.benchmark.templateengine.ResourceConfig;
 
-@ContextConfiguration(classes =  ResourceConfig.class)
-public abstract class  ExpectedOutputTest  extends SetupTest {
+import java.util.Locale;
 
-    void assertOutput(final String process, final String output) {
-        assertEquals(output.replaceAll("\\s", ""), process.replaceAll("\\s", ""));
+@RunWith(SpringRunner.class)
+@EnableJpaRepositories(basePackages = "ru.sbertech.platformv.print.benchmark.domain.repository")
+@EntityScan(basePackages = "ru.sbertech.platformv.print.benchmark.domain.model.entity")
+@SpringBootTest(classes = {OfficeMapperImpl.class,
+        EmployeeMapperImpl.class,
+        ResourceResolverService.class,
+        ProjectRepository.class,
+        OfficeRepository.class,
+        CompanyRepository.class,
+        CompanyMapperImpl.class,
+        CompanyService.class,
+        EmployeeRepository.class, OfficeService.class})
+@Testcontainers
+@EnableAutoConfiguration(exclude = {BenchmarkTemplateEnginesAutoConfiguration.class})
+public abstract class SetupTest {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:12.19");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("database.url", postgres::getJdbcUrl);
+        registry.add("database.username", postgres::getUsername);
+        registry.add("database.password", postgres::getPassword);
+        registry.add("database.driver", postgres::getDriverClassName);
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        Locale.setDefault(Locale.ENGLISH);
     }
 }
---
+++ benchmark/src/test/resources/templates/report/velocity/company.pm
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Companies Report</title>
+</head>
+<link rel="stylesheet" type="text/css" href="../../style.css">
+<body>
+<h1>Companies Report</h1>
+#foreach($company in $companies)
+<div class="company-section">
+    <div class="company-info"><h2>$company.name</h2>
+        <p><strong>Code:</strong> $company.code</p>
+        <p><strong>Director:</strong> $company.director.name</p>
+        <p><strong>Main office:</strong> $company.generalOffice.name</p></div>
+    <div class="company-logo">
+        <img src="../../assets/company/$company.logo.name" alt="$company.name logo"/>
+    </div>
+</div>
+<h3>Additional Offices</h3>
+#foreach($office in $company.additionalOffices)
+<section class="office-section">
+    <section class="office-info-section">
+        <div class="office-info">
+            <h4>$office.name</h4>
+            <p><strong>Location:</strong> $office.location</p>
+
+            #if($office.resources)
+            <div>
+                <strong>Resources:</strong>
+                #foreach($resource in $office.resources)
+                #set ($mod = $foreach.index % 3)
+                <p class="resources" style="background-color: $colors[$mod];">
+                    $resource
+                </p>
+                #end
+            </div>
+            #end
+        </div>
+        <div class="office-photo">
+            <img src="../../assets/office/$office.photo.name" alt="office.name logo"/>
+        </div>
+    </section>
+    <table>
+        <thead>
+        <tr>
+            <th>Personal Table</th>
+            <th>Name</th>
+            <th>Project</th>
+            <th>Position</th>
+            <th>Salary</th>
+            <th>Experience (years)</th>
+        </tr>
+        </thead>
+        <tbody>
+        #foreach($employee in $office.employees)
+        <tr>
+            <td>$employee.id</td>
+            <td>$employee.name</td>
+            <td>$employee.project.name</td>
+            <td>$employee.position</td>
+            <td>$employee.salary</td>
+            <td>$employee.experience</td>
+        </tr>
+        #end
+        </tbody>
+    </table>
+</section>
+#end
+#end
+</body>
+</html>
